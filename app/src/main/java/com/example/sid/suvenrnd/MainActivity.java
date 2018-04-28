0bdc29ade5afddb15a8ce383bd3e72021f978ba8
sidgwd@gmail.com <Siddragan1>
2018-04-28 16:59:20 +0530

    Very first commit

---
+++ app/src/main/java/com/example/sid/suvenrnd/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.sid.suvenrnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    String varialbele1, variable2, variable3;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        varialbele1 = "";
+        variable2 = "";
+        variable3 = "";
+    }
+}
